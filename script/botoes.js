ce71cb9a7cb14e8bf8e90c405b16cb90693ad21d
Vinícius Leonel <124933333+viniciusleonel@users.noreply.github.com>
2023-03-20 20:42:37 -0300

    Update botoes.js

--- script/botoes.js
+++ script/botoes.js
@@ -1,12 +1,13 @@
 let gui;
 let b;
 let c;
-
+let f;
 
 function drawBotao(){
     gui = createGui();
     b = createButton("Up", 100, 740, 200, 50);
     c = createButton("Down", 700, 740, 200, 50);
+    f = createButton("FullScreen", 700, 10, 200, 50);
 }
 
 function botao(){
@@ -17,6 +18,10 @@ function botao(){
       }
       if (b.isPressed){
         yAtor -= 80;
+      }
+      if (f.isPressed){
+         let fs = fullscreen();
+         fullscreen(!fs);        
       }
     }
 
